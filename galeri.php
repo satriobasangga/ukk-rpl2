a08c4ba5b74e4f4257a5a255f82045a7b5e2baf4
lombokonlinesusanti <lombokonlinesusanty@gmail.com>
2024-02-20 16:58:30 +0800

    membuat gallery foto

---
+++ galeri.php
@@ -0,0 +1,58 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+<a href="">login</a> | <a href="">logout</a> 
+
+
+    <H1>HALAMAN galeri</H1> 
+    <a href="">tambah data </a>
+    <table border=1>
+        <thead>
+            <tr>
+                <th><h1>galery_album</h1></th>
+                <th>albumid</th>
+                <th>namaalbum</th>
+                <th>deskripsi</th>
+                <th>tanggaldibuat</th>
+                <th>userid</th>
+            </tr>    
+            <tr>
+                <th><h1>galeri_foto</h1></th>
+                <th>fotoid</th>
+                <th>judulfoto</th>
+                <th>deskripsifoto</th>
+                <th>tanggaldiunggah</th>
+                <th>lokasifile</th>
+                <th>albumid</th>
+                <th>userid</th>
+            </tr>
+            <tr>
+                <th><h1>komentarfoto</h1></th>
+                <th>komentarid</th>
+                <th>fotoid</th>
+                <th>userid</th>
+                <th>isikomentar</th>
+                <th>tanggalkomentar</th>
+            </tr>    
+
+         </thead>
+         <tbody>
+            <?php 
+             ?>
+            <tr>
+
+            
+             </td>
+ 
+          </tr>
+          <?php 
+
+           ?>
+       </tbody>
+    </table>
+</body>
+</html>
